ef0b5a356e01db3a911948262458f1b5e7238b84
Mauricio Guerrero <maurizioguerrero@hotmail.com>
2023-06-05 18:36:05 -0500

    importing old data

--- src/routes/Contacto/Contacto.jsx
+++ src/routes/Contacto/Contacto.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import "./Contacto.css";
+import { useRef } from "react";
+import { MdOutlineMail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Contacto = () => {
-    return (
-        <div>
-            Contacto
-        </div>
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_fnzporp",
+      "template_sbj5wdj",
+      form.current,
+      "7pQTIDrFG3Z9aeQET"
     );
-}
+    e.target.reset();
+  };
+  return (
+    <>
+      <h2 className="titulo">Contacto</h2>
+
+      <div className="contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>galeras57@gmail.com</h5>
+            <a href="mailto: sgbioarq@gmail.com">Enviar correo</a>
+          </article>
+
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <a href="https://api.whatsapp.com/send?phone=573137675461">
+              Enviar mensaje
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="text" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+      </>
+  );
+};
 
 export default Contacto;
--- src/routes/Servicios/Servicios.jsx
+++ src/routes/Servicios/Servicios.jsx
@@ -2,9 +2,88 @@ import React from 'react';
 
 const Servicios = () => {
     return (
-        <div>
-            Servicios
+        <>
+        <section className="portafolio" id="portafolio">
+        <div className="contenedor">
+            <h2 className="titulo">Portafolio de Servicios</h2>
+            <div className="galeria-port">
+                <div className="imagen-port">
+                    <p>Consulta Medica Homeopática</p>
+                    <img src="./assets/images/servicios1.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>La homeopatía es un modelo médico clínico terapéutico que
+                            utiliza sustancias obtenidas de vegetales, animales y minerales principalmente, en
+                            concentraciones muy pequeñas disueltas en agua y alcohol. Los síntomas muestran el
+                            esfuerzo curativo del cuerpo, en este sentido el medicamento homeopático respeta al
+                            organismo y se encarga de modular o regular los padecimientos en lugar de disfrazarlos.
+                        </p>
+                    </div>
+                </div>
+                <div className="imagen-port">
+                    <p>Consulta Medicina tradicional China</p>
+                    <img src="./assets/images/servicios2.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>La medicina tradicional china trata de recuperar el
+                            equilibrio del cuerpo y la armonía entre las fuerzas naturales opuestas del ying y el
+                            yang, que pueden bloquear el qi y causar enfermedad. La medicina tradicional china
+                            incluye acupuntura, régimen de alimentación, terapia con hierbas, meditación, ejercicio
+                            físico y masaje. También se llama medicina oriental y MTC.</p>
+                    </div>
+                </div>
+                <div className="imagen-port">
+                    <p>Terapia Neural</p>
+                    <img src="./assets/images/servicios3.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>La terapia neural consiste en la inyección de dosis muy bajas
+                            de anestésico local en determinadas zonas del cuerpo con fines terapéuticos. Se
+                            considera una forma de medicina alternativa, indicada especialmente en el tratamiento
+                            del dolor y las enfermedades crónicas. Su principal valor es que no presenta efectos
+                            secundarios ni suele tener complicaciones, y suele realizarse con la procaína como
+                            anestésico local.</p>
+                    </div>
+                </div>
+                <div className="imagen-port">
+                    <p>Acupuntura</p>
+                    <img src="./assets/images/servicios4.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>La acupuntura describe una familia de procedimientos que
+                            estimulan diferentes puntos del cuerpo utilizando una variedad de técnicas. La técnica
+                            de acupuntura que se estudia científicamente con mayor frecuencia consiste en colocar
+                            agujas metálicas sólidas, delgadas, estériles en la piel y manipularlas.</p>
+                    </div>
+                </div>
+                
+                <div className="imagen-port">
+                    <p>Terapia con Sueros</p>
+                    <img src="./assets/images/servicios5.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>Tratamiento consistente en la administración de sueros
+                            específicos que favorecen la autocuración de nuestro organismo a partir de una selección
+                            de vitaminas, oligoelementos y fármacos biológicos naturales según nuestras necesidades
+                            concretas: adelgazar, embellecer nuestra piel, mejorar la salud de nuestro cabello,
+                            etcétera.</p>
+                    </div>
+                </div>
+                <div className="imagen-port">
+                    <p>Auriculoterapia</p>
+                    <img src="./assets/images/servicios6.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>La auriculoterapia es una técnica de uso de puntos en la piel
+                            de la aurícula (oído externo) para diagnosticar y tratar el dolor.</p>
+                    </div>
+                </div>
+
+                <div className="imagen-port">
+                    <p>Electroacupuntura</p>
+                    <img src="./assets/images/servicios7.jpg" alt="" loading="lazy"/>
+                    <div className="hover-galeria">
+                        <p>Mediante la aplicacion correcta de una corriente electrica adaptada para uso medico se potencia el efecto de la acupuntura tradicional reduciendo molestias para el paciente, numero de sesiones de tratamiento y mayor eficacia en el control del dolor cronico causado por diferentes patologias como arthrosis, artritis, fibtomialgia, enfermedad reumatica.</p>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+    </>
     );
 }
 
