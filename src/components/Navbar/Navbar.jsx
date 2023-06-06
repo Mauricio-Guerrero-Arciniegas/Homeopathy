a8c9203839981f6eda41f736417728a4f08fdb50
Mauricio Guerrero <maurizioguerrero@hotmail.com>
2023-06-06 12:44:50 -0500

    wsp-done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,23 +11,16 @@ const Navbar = () => {
 
   return (
     <nav className="nav">
-      <div className="btn-wsp">
-          
-        </div>
       <div className="nav__container">
         
         <h1 className="nav__logo">
-        <a
-            href="https://api.whatsapp.com/send?phone=3137675461"
-            className="btn-wsp"
-            target="_blank"
-          >
-            <img
+        <a href="https://api.whatsapp.com/send?phone=3137675461"className="btn-wsp"target="_blank">
+          <img
               className="btn-wsp"
               src="./assets/icons/whatsapp.png"
               alt=""
               loading="lazy"
-            />
+            /> 
           </a>
           Dr. Mauricio Guerrero R.
           </h1>
